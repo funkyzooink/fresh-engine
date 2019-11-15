588ea8cc37b892905b1d0005b5f7d676f2d2d8f1
funkyzooink <funkyzooink@gmail.com>
2019-11-15 20:17:10 +0100

    another test

--- .travis/tag.py
+++ .travis/tag.py
@@ -2,13 +2,15 @@
 
 import os
 
-tagname = os.environ["TRAVIS_TAG"]
+if os.environ.get('TRAVIS_TAG'):
+    tagname = os.environ["TRAVIS_TAG"]
 
-if "little-ninja" in tagname:
-    os.environ["SUBMODULE_PATH"] = "examples/little-ninja/"
-elif "the-dragon-kid" in tagname:
-    os.environ["SUBMODULE_PATH"] = "examples/the-dragon-kid/"
-elif "4friends" in tagname:
-    os.environ["SUBMODULE_PATH"] = "examples/4friends/"
+    if "little-ninja" in tagname:
+        os.environ["SUBMODULE_PATH"] = "examples/little-ninja/"
+    elif "the-dragon-kid" in tagname:
+        os.environ["SUBMODULE_PATH"] = "examples/the-dragon-kid/"
+    elif "4friends" in tagname:
+        os.environ["SUBMODULE_PATH"] = "examples/4friends/"
 
-print(os.environ["SUBMODULE_PATH"])
+if os.environ.get('SUBMODULE_PATH'):
+    print(os.environ["SUBMODULE_PATH"])
