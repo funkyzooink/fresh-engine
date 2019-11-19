fb6332fd5912ae63179e7291c848ae51ffa495d4
funkyzooink <funkyzooink@gmail.com>
2019-11-19 18:16:38 +0100

    tag script update

--- create_tags.sh
+++ create_tags.sh
@@ -2,5 +2,6 @@
 
 version=$(git rev-list HEAD --count)
 git tag "release-little-ninja-v$version"
+git tag "release-little-robot-adventure-v$version"
 git tag "release-the-dragon-kid-v$version"
 git push origin --tags
